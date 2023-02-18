3435927813a51229085533a6069854bdaef657ef
Matteo Vitali <matteo@20tab.com>
2023-02-18 18:09:46 +0100

    Improve survey_list data

--- src/api.rs
+++ src/api.rs
@@ -8,7 +8,14 @@ use futures::{future::ok, stream::once};
 
 
 pub async fn surveys_list(_req: HttpRequest) -> HttpResponse {
-    let body = once(ok::<_, Error>(web::Bytes::from_static(b"[{\"id\": 1, \"name\": \"Ownership & Empowerment\", \"slug\": \"ownership_empowerment\"},{\"id\": 2, \"name\": \"Teamwork & Alignment\", \"slug\": \"teamwork_alignment\"},{\"id\": 3, \"name\": \"Safety to take a risk\", \"slug\": \"safety_take_risk\"},{\"id\": 4, \"name\": \"Pride\", \"slug\": \"pride\"},{\"id\": 5, \"name\": \"Fun & Joy\", \"slug\": \"fun_joy\"}]")));
+    let data = b"[
+{\"id\": 1, \"name\": \"Ownership & Empowerment\", \"slug\": \"ownership_empowerment\"},
+{\"id\": 2, \"name\": \"Teamwork & Alignment\", \"slug\": \"teamwork_alignment\"},
+{\"id\": 3, \"name\": \"Safety to take a risk\", \"slug\": \"safety_take_risk\"},
+{\"id\": 4, \"name\": \"Pride\", \"slug\": \"pride\"},
+{\"id\": 5, \"name\": \"Fun & Joy\", \"slug\": \"fun_joy\"}
+]";
+    let body = once(ok::<_, Error>(web::Bytes::from_static(data)));
     HttpResponse::Ok()
         .content_type("application/json")
         .streaming(body)
@@ -27,7 +34,15 @@ mod tests {
         let resp = app.call(req).await.unwrap();
         assert!(resp.status().is_success());
         let response_body_bytes = to_bytes(resp.into_body()).await.unwrap();
-        assert_eq!(to_bytes(response_body_bytes).await.unwrap(), "[{\"id\": 1, \"name\": \"Ownership & Empowerment\", \"slug\": \"ownership_empowerment\"},{\"id\": 2, \"name\": \"Teamwork & Alignment\", \"slug\": \"teamwork_alignment\"},{\"id\": 3, \"name\": \"Safety to take a risk\", \"slug\": \"safety_take_risk\"},{\"id\": 4, \"name\": \"Pride\", \"slug\": \"pride\"},{\"id\": 5, \"name\": \"Fun & Joy\", \"slug\": \"fun_joy\"}]");
+        assert_eq!(
+            to_bytes(response_body_bytes).await.unwrap(), 
+            "[
+{\"id\": 1, \"name\": \"Ownership & Empowerment\", \"slug\": \"ownership_empowerment\"},
+{\"id\": 2, \"name\": \"Teamwork & Alignment\", \"slug\": \"teamwork_alignment\"},
+{\"id\": 3, \"name\": \"Safety to take a risk\", \"slug\": \"safety_take_risk\"},
+{\"id\": 4, \"name\": \"Pride\", \"slug\": \"pride\"},
+{\"id\": 5, \"name\": \"Fun & Joy\", \"slug\": \"fun_joy\"}
+]");
         Ok(())
     }
 }
--- src/index.rs
+++ src/index.rs
@@ -46,5 +46,4 @@ mod tests {
         Ok(())
     }
 
-
 }
